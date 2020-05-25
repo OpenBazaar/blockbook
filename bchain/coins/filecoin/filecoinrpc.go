d28c0d93d0e1dfda082485c586645a945d1c0838
Chris <chris@ob1.io>
2020-05-24 21:14:40 -0400

    Second pass rpc

--- bchain/coins/filecoin/filecoinrpc.go
+++ bchain/coins/filecoin/filecoinrpc.go
@@ -1,9 +1,16 @@
 package filecoin
 
 import (
+	"bytes"
 	"context"
+	"encoding/hex"
 	"encoding/json"
+	"errors"
+	"github.com/filecoin-project/go-address"
+	"github.com/filecoin-project/lotus/api"
 	"github.com/filecoin-project/lotus/api/apistruct"
+	"github.com/filecoin-project/lotus/chain/types"
+	"github.com/filecoin-project/specs-actors/actors/abi"
 	"github.com/golang/glog"
 	"github.com/ipfs/go-cid"
 	"github.com/multiformats/go-multiaddr"
@@ -30,13 +37,16 @@ type FilecoinRPC struct {
 	*bchain.BaseChain
 	fullNode *apistruct.FullNodeStruct
 
-	Parser      *FilecoinParser
-	Testnet     bool
-	Network     string
-	ChainConfig *Configuration
-	Mempool     *bchain.MempoolFilecoinType
+	Parser             *FilecoinParser
+	Testnet            bool
+	Network            string
+	ChainConfig        *Configuration
+	Mempool            *bchain.MempoolFilecoinType
+	mempoolInitialized bool
+	pushHandler        func(notificationType bchain.NotificationType)
 
-	close func()
+	shutdown chan struct{}
+	close    func()
 }
 
 // NewFilecoinRPC returns new FilecoinRPC instance.
@@ -51,6 +61,8 @@ func NewFilecoinRPC(config json.RawMessage, pushHandler func(bchain.Notification
 		ChainConfig: &cfg,
 		Parser:      NewFilecoinParser(&cfg),
 		BaseChain:   &bchain.BaseChain{},
+		pushHandler: pushHandler,
+		shutdown:    make(chan struct{}),
 	}, nil
 }
 
@@ -67,6 +79,25 @@ func (f *FilecoinRPC) Initialize() error {
 	}
 	f.fullNode = fullNode
 	f.close = close
+
+	chainChan, err := fullNode.ChainNotify(context.Background())
+	if err != nil {
+		return err
+	}
+
+	// TODO: how do we subscribe to pending transactions from the full node?
+
+	go func() {
+		for {
+			select {
+			case <-chainChan:
+				f.pushHandler(bchain.NotificationNewBlock)
+			case <-f.shutdown:
+				return
+			}
+		}
+	}()
+
 	return nil
 }
 
@@ -80,30 +111,63 @@ func (f *FilecoinRPC) CreateMempool(chain bchain.BlockChain) (bchain.Mempool, er
 }
 
 // initialize mempool, create ZeroMQ (or other) subscription
-func (f *FilecoinRPC) InitializeMempool(bchain.AddrDescForOutpointFunc, bchain.OnNewTxAddrFunc) error {
-	// TODO
+func (f *FilecoinRPC) InitializeMempool(addrDescForOutpoint bchain.AddrDescForOutpointFunc, onNewTxAddr bchain.OnNewTxAddrFunc) error {
+	if f.Mempool == nil {
+		return errors.New("mempool not created")
+	}
+
+	// get initial mempool transactions
+	txs, err := f.GetMempoolTransactions()
+	if err != nil {
+		return err
+	}
+	for _, txid := range txs {
+		f.Mempool.AddTransactionToMempool(txid)
+	}
+
+	f.Mempool.OnNewTxAddr = onNewTxAddr
+
+	f.mempoolInitialized = true
+
 	return nil
 }
 
 // shutdown mempool, ZeroMQ and block chain connections
 func (f *FilecoinRPC) Shutdown(ctx context.Context) error {
 	f.close()
+	close(f.shutdown)
 	return nil
 }
 
 func (f *FilecoinRPC) GetSubversion() string {
-	// TODO
-	return ""
+	ver, _ := f.fullNode.Version(context.Background())
+	return ver.String()
 }
 
 func (f *FilecoinRPC) GetCoinName() string {
-	// TODO
-	return ""
+	return f.ChainConfig.CoinName
 }
 
 func (f *FilecoinRPC) GetChainInfo() (*bchain.ChainInfo, error) {
-	// TODO
-	return nil, nil
+	hash, err := f.GetBestBlockHash()
+	if err != nil {
+		return nil, err
+	}
+	height, err := f.GetBestBlockHeight()
+	if err != nil {
+		return nil, err
+	}
+	ver, _ := f.fullNode.Version(context.Background())
+
+	// TODO: difficuty, protoco version, size on disk, timeoffset, warnings?
+	return &bchain.ChainInfo{
+		Blocks:        int(height),
+		Bestblockhash: hash,
+		Headers:       int(height),
+		Subversion:    f.GetSubversion(),
+		Version:       ver.APIVersion.String(),
+		Chain:         f.ChainConfig.CoinName,
+	}, nil
 }
 
 func (f *FilecoinRPC) GetBestBlockHash() (string, error) {
@@ -127,7 +191,12 @@ func (f *FilecoinRPC) GetBestBlockHeight() (uint32, error) {
 }
 
 func (f *FilecoinRPC) GetBlockHash(height uint32) (string, error) {
-	return "", nil
+	// TODO: what do we use for tipset key here
+	tipSet, err := f.fullNode.ChainGetTipSetByHeight(context.Background(), abi.ChainEpoch(height), types.TipSetKey{})
+	if err != nil {
+		return "", err
+	}
+	return tipSet.Cids()[0].String(), nil
 }
 
 func (f *FilecoinRPC) GetBlockHeader(hash string) (*bchain.BlockHeader, error) {
@@ -143,61 +212,159 @@ func (f *FilecoinRPC) GetBlockHeader(hash string) (*bchain.BlockHeader, error) {
 	if err != nil {
 		return nil, err
 	}
+	// TODO: confirmations, size, next?
 	ret := &bchain.BlockHeader{
 		Height: uint32(height),
 		Time:   int64(header.Timestamp),
 		Hash:   header.Cid().String(),
+		Prev:   header.Parents[0].String(),
 	}
-	// TODO: there are other fields that need to be added to the header
 	return ret, nil
 }
 
 func (f *FilecoinRPC) GetBlock(hash string, height uint32) (*bchain.Block, error) {
-	// TODO
-	return nil, nil
+	var blkMessages *api.BlockMessages
+	if hash != "" {
+		h, err := cid.Decode(hash)
+		if err != nil {
+			return nil, err
+		}
+		blkMessages, err = f.fullNode.ChainGetBlockMessages(context.Background(), h)
+		if err != nil {
+			return nil, err
+		}
+	} else {
+		tipSet, err := f.fullNode.ChainGetTipSetByHeight(context.Background(), abi.ChainEpoch(height), types.TipSetKey{})
+		if err != nil {
+			return nil, err
+		}
+		blkMessages, err = f.fullNode.ChainGetBlockMessages(context.Background(), tipSet.Cids()[0])
+		if err != nil {
+			return nil, err
+		}
+	}
+	// TODO: this seems like an inefficient way to get the block txs. Is there an
+	// API call to get everything at once?
+	txs := make([]bchain.Tx, len(blkMessages.Cids))
+	for i, id := range blkMessages.Cids {
+		tx, err := f.GetTransaction(id.String())
+		if err != nil {
+			return nil, err
+		}
+		txs[i] = *tx
+
+		if f.mempoolInitialized {
+			f.Mempool.RemoveTransactionFromMempool(tx.Txid)
+		}
+	}
+	blk := &bchain.Block{
+		Txs: txs,
+	}
+	return blk, nil
 }
 
 func (f *FilecoinRPC) GetBlockInfo(hash string) (*bchain.BlockInfo, error) {
-	// TODO
-	return nil, nil
+	h, err := cid.Decode(hash)
+	if err != nil {
+		return nil, err
+	}
+	header, err := f.GetBlockHeader(hash)
+	if err != nil {
+		return nil, err
+	}
+	blkHeader, err := f.fullNode.ChainGetBlock(context.Background(), h)
+	if err != nil {
+		return nil, err
+	}
+	messages, err := f.fullNode.ChainGetBlockMessages(context.Background(), h)
+	if err != nil {
+		return nil, err
+	}
+	txids := make([]string, len(messages.Cids))
+	for i, id := range messages.Cids {
+		txids[i] = id.String()
+	}
+	// TODO: verison, difficult, nonce, bits?
+	bi := &bchain.BlockInfo{
+		MerkleRoot:  blkHeader.Messages.String(), // TODO: is this right?
+		Txids:       txids,
+		BlockHeader: *header,
+	}
+	return bi, nil
 }
 
 func (f *FilecoinRPC) GetMempoolTransactions() ([]string, error) {
-	// TODO
-	return nil, nil
+	// TODO: what do we use for tipsetkey here?
+	msgs, err := f.fullNode.MpoolPending(context.Background(), types.TipSetKey{})
+	if err != nil {
+		return nil, err
+	}
+	txids := make([]string, len(msgs))
+	for i, msg := range msgs {
+		txids[i] = msg.Cid().String()
+	}
+	return txids, nil
 }
 
 func (f *FilecoinRPC) GetTransaction(txid string) (*bchain.Tx, error) {
-	// TODO
-	return nil, nil
+	h, err := cid.Decode(txid)
+	if err != nil {
+		return nil, err
+	}
+	message, err := f.fullNode.ChainGetMessage(context.Background(), h)
+	if err != nil {
+		return nil, err
+	}
+	// TODO: Figure out how to get the blocktime and height
+	return f.Parser.filMessageToTx(message, 0, 0)
 }
 
 func (f *FilecoinRPC) GetTransactionForMempool(txid string) (*bchain.Tx, error) {
-	// TODO
-	return nil, nil
+	return f.GetTransaction(txid)
 }
 
 func (f *FilecoinRPC) GetTransactionSpecific(tx *bchain.Tx) (json.RawMessage, error) {
-	// TODO
-	return nil, nil
+	csd, ok := tx.CoinSpecificData.(*types.Message)
+	if !ok {
+		ntx, err := f.GetTransaction(tx.Txid)
+		if err != nil {
+			return nil, err
+		}
+		csd, ok = ntx.CoinSpecificData.(*types.Message)
+		if !ok {
+			return nil, errors.New("cannot get CoinSpecificData")
+		}
+	}
+	m, err := json.Marshal(&csd)
+	return json.RawMessage(m), err
 }
 
 func (f *FilecoinRPC) EstimateSmartFee(blocks int, conservative bool) (big.Int, error) {
-	// TODO
-	return big.Int{}, nil
+	// TODO: what do we use for address, limit, and tipsetkey here?
+	fee, err := f.fullNode.MpoolEstimateGasPrice(context.Background(), uint64(blocks), address.Address{}, 0, types.TipSetKey{})
+	if err != nil {
+		return big.Int{}, err
+	}
+	return *fee.Int, nil
 }
 
 func (f *FilecoinRPC) EstimateFee(blocks int) (big.Int, error) {
-	// TODO
-	return big.Int{}, nil
+	return f.EstimateSmartFee(blocks, true)
 }
 
 func (f *FilecoinRPC) SendRawTransaction(tx string) (string, error) {
-	// TODO
-	return "", nil
-}
+	b, err := hex.DecodeString(tx)
+	if err != nil {
+		return "", err
+	}
 
-func (f *FilecoinRPC) GetMempoolEntry(txid string) (*bchain.MempoolEntry, error) {
-	// TODO
-	return nil, nil
+	var msg types.SignedMessage
+	if err := msg.UnmarshalCBOR(bytes.NewReader(b)); err != nil {
+		return "", err
+	}
+	id, err := f.fullNode.MpoolPush(context.Background(), &msg)
+	if err != nil {
+		return "", err
+	}
+	return id.String(), nil
 }
